56781273ef97d2daa5c87336514a353ee715aac1
Ranbir Aulakh <ranbiraulakhme@gmail.com>
2024-08-21 15:46:32 +0000

    fix: change put_object to upload_file to improve performance

--- src/aws/osml/model_runner/sink/s3_sink.py
+++ src/aws/osml/model_runner/sink/s3_sink.py
@@ -2,10 +2,12 @@
 
 import logging
 import os
+import tempfile
 from typing import List, Optional
 
 import boto3
 import geojson
+from boto3.s3.transfer import TransferConfig
 from botocore.exceptions import ClientError
 from geojson import Feature, FeatureCollection
 
@@ -55,14 +57,26 @@ class S3Sink(Sink):
             # want to base our key off of the original image file name so split by '/' and use
             # the last element
             object_key = os.path.join(self.prefix, image_id.split("/")[-1] + ".geojson")
-            # Add the aggregated features to a feature collection and encode the full set of features
-            # as a GeoJSON output.
-            self.s3_client.put_object(
-                Body=str(geojson.dumps(features_collection)),
-                Bucket=self.bucket,
-                Key=object_key,
-                ACL="bucket-owner-full-control",
-            )
+
+            # Create a temporary file to store aggregated features as a GeoJSON data
+            with tempfile.NamedTemporaryFile(delete=True) as temp_file:
+                with open(temp_file.name, "w") as f:
+                    f.write(geojson.dumps(features_collection))
+
+                # Use upload_file to upload the file to S3
+                self.s3_client.upload_file(
+                    Filename=temp_file.name,
+                    Bucket=self.bucket,
+                    Key=object_key,
+                    Config=TransferConfig(
+                        multipart_threshold=64 * 1024**2,  # 64 MB
+                        max_concurrency=10,
+                        multipart_chunksize=128 * 1024**2,  # 128 MB
+                        use_threads=True,
+                    ),
+                    ExtraArgs={"ACL": "bucket-owner-full-control"},
+                )
+
             logger.info(f"Wrote aggregate feature collection for Image '{image_id}' to s3://{self.bucket}/{object_key}")
             return True
         else:
