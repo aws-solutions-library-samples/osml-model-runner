53dc501dfccf79137c88882f2f83725c224504e8
drduhe <drduhe@amazon.com>
2024-10-25 15:22:13 -0600

    test: updating inference submodule test structure and comments

--- test/aws/osml/model_runner/inference/test_endpoint_factory.py
+++ test/aws/osml/model_runner/inference/test_endpoint_factory.py
@@ -5,6 +5,10 @@ import unittest
 
 class TestFeatureDetectorFactory(unittest.TestCase):
     def test_sm_detector_generation(self):
+        """
+        Test that the FeatureDetectorFactory correctly creates an SMDetector
+        when the endpoint mode is set to ModelInvokeMode.SM_ENDPOINT.
+        """
         from aws.osml.model_runner.api.inference import ModelInvokeMode
         from aws.osml.model_runner.inference import FeatureDetectorFactory, SMDetector
 
@@ -12,10 +16,16 @@ class TestFeatureDetectorFactory(unittest.TestCase):
             endpoint="test",
             endpoint_mode=ModelInvokeMode.SM_ENDPOINT,
         ).build()
+
+        # Verify that the detector is an instance of SMDetector and has the correct mode
         assert isinstance(feature_detector, SMDetector)
         assert feature_detector.mode == ModelInvokeMode.SM_ENDPOINT
 
     def test_http_detector_generation(self):
+        """
+        Test that the FeatureDetectorFactory correctly creates an HTTPDetector
+        when the endpoint mode is set to ModelInvokeMode.HTTP_ENDPOINT.
+        """
         from aws.osml.model_runner.api.inference import ModelInvokeMode
         from aws.osml.model_runner.inference import FeatureDetectorFactory, HTTPDetector
 
@@ -23,5 +33,11 @@ class TestFeatureDetectorFactory(unittest.TestCase):
             endpoint="test",
             endpoint_mode=ModelInvokeMode.HTTP_ENDPOINT,
         ).build()
+
+        # Verify that the detector is an instance of HTTPDetector and has the correct mode
         assert isinstance(feature_detector, HTTPDetector)
         assert feature_detector.mode == ModelInvokeMode.HTTP_ENDPOINT
+
+
+if __name__ == "__main__":
+    unittest.main()
